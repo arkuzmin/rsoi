deecb0a0532424c605308a81760d451834e5b062
ArKuzmin <KuzminArtY@gmail.com>
2013-05-16 00:27:37 +0400

    Добавил АИС таксопарка

---
+++ src/TaxiParkWeb/src/main/java/ru/arkuzmin/tpw/common/CommonUtils.java
@@ -0,0 +1,10 @@
+package ru.arkuzmin.tpw.common;
+
+import org.apache.log4j.Logger;
+
+public class CommonUtils {
+	
+	public static Logger getLogger() {
+		return Logger.getLogger("TaxiParkWeblogger");
+	}
+}
---
+++ src/TaxiParkWeb/src/main/java/ru/arkuzmin/tpw/listener/DispAISListener.java
@@ -0,0 +1,27 @@
+package ru.arkuzmin.tpw.listener;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+import org.apache.log4j.Logger;
+
+import ru.arkuzmin.tpw.common.CommonUtils;
+
+public class DispAISListener implements MessageListener {
+
+	private static final Logger logger = CommonUtils.getLogger();
+
+	@Override
+	public void onMessage(Message msg) {
+		try {
+			if (msg instanceof TextMessage) {
+				TextMessage txtMsg = (TextMessage) msg;
+				logger.debug("Получено сообщение: " + txtMsg.getText());
+			}
+		} catch (JMSException e) {
+			logger.error("Error", e);
+		}
+	}
+}
