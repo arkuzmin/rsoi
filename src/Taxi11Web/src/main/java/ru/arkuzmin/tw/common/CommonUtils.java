528e881703e72d1eca012d5b4bbd59cc971dd59f
ArKuzmin <KuzminArtY@gmail.com>
2013-05-31 22:46:12 +0400

    развернуты новые системы такси
    +правки к рпз и презентации

---
+++ src/Taxi11Web/src/main/java/ru/arkuzmin/tw/common/CommonUtils.java
@@ -0,0 +1,10 @@
+package ru.arkuzmin.tw.common;
+
+import org.apache.log4j.Logger;
+
+public class CommonUtils {
+	
+	public static Logger getLogger() {
+		return Logger.getLogger("Taxi1Taxipark1Weblogger");
+	}
+}
